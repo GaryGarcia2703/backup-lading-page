5335bfa4c71e651d422dcca5a4845ce2342767ed
Gary_garcia <Gary.Garcia2703@gmail.com>
2026-01-20 13:43:23 -0300

    cta-form update

---
+++ src/js/cta.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const openBtn = document.getElementById("open-cta");
+  const overlay = document.getElementById("cta-overlay");
+  const closeBtn = document.querySelector(".cta-close");
+
+  // ===== ABRIR MODAL =====
+  openBtn.addEventListener("click", () => {
+    overlay.classList.add("show");
+    document.body.style.overflow = "hidden";
+  });
+
+  // ===== CERRAR MODAL =====
+  closeBtn.addEventListener("click", () => {
+    overlay.classList.remove("show");
+    document.body.style.overflow = "";
+  });
+
+  overlay.addEventListener("click", (e) => {
+    if (e.target === overlay) {
+      overlay.classList.remove("show");
+      document.body.style.overflow = "";
+    }
+  });
+
+});
