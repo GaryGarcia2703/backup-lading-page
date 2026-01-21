0445b67d1e168f8f5432ab331b8097e1fdb71ecf
Gary_garcia <Gary.Garcia2703@gmail.com>
2026-01-21 10:45:53 -0300

    logo_upd

--- src/js/transition.js
+++ src/js/transition.js
@@ -13,7 +13,7 @@ window.addEventListener('scroll', () => {
     `;
 });
 
-// transicião suave ao descer mudando o nav
+// trancision suave al hacer scroll
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
@@ -22,4 +22,17 @@ window.addEventListener('scroll', () => {
     } else {
         header.classList.remove('scrolled');
     }
+});
+
+// cambio de logo azul a blanco al hacer scroll
+const logo = document.getElementById("navbar-logo");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 520) {
+    header.classList.add("scrolled");
+    logo.src = "src/img/logo_blanco.png"; // logo para fundo claro
+  } else {
+    header.classList.remove("scrolled");
+    logo.src = "src/img/logo_azul.png"; // logo original
+  }
 });
